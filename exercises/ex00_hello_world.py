aea21831935ea20e41198925e5eab15dbf47ad4c
Kaviya David <kavs@live.unc.edu>
2021-08-24 14:35:40 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,4 @@
-"""My first program for COMP110"""
+"""My first program for COMP110!"""
 
-_730316359_= "1234567890"
-
-print("Hello to the whole world.")
+_author_= "730316359"
+print( "Hello to the whole world." )
