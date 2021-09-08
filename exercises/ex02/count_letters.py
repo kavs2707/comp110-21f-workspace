a771e78b0056275ed37a6f4f10418bd4ebeef505
Kaviya David <kavs@live.unc.edu>
2021-09-08 18:44:35 -0400

    Progress on EX02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -3,10 +3,13 @@
 __author__ = "730316359"
 
 
-letter: str = input("What letter do you want to search for?: ")
+what_letter: str = input("What letter do you want to search for?: ")
 word: str = input("Enter a word: ")
 
 i: int = 0
 
-while i < len(word):
-    
+while i < len(word): 
+    letters: str = (what_letter[i])
+    if word == letters:
+       print(word == letters) 
+    i = i + 1
