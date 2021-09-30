1b4457d28dfdc2cde022f262fad119df03ead700
Kaviya David <kavs@live.unc.edu>
2021-09-30 11:50:43 -0400

    Clearing SCM

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -4,16 +4,13 @@ __author__ = "730316359"
 
 
 
-what_letter: str = input("What letter do you want to search for?: ")
+letter: str = input("What letter do you want to search for?: ")
 word: str = input("Enter a word: ")
-count: str = ""
-i: int = 0
 
-if what_letter != word[i]:
-    print("0")
-
-while i < len(word): 
-    if what_letter == word[i]:
-        i = i + 1
-        count = i
-
+counter: int = 0
+while counter < len(word): 
+    if letter == word[counter]:
+        count = len(word[counter])
+        counter = counter + 1
+        count = len(word[counter]) + 1
+        print(count)
