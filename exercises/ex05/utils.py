6e0a6988fc61ef42edbb6b719c3df858ba62c42d
Kaviya David <kavs@live.unc.edu>
2021-10-05 22:42:53 -0400

    EX05 Final

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,50 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730316359"
 
 # Define your functions below
+
+
+def only_evens(x: list[int]) -> list[int]:
+    """This function returns a list containing only the even elements of the input list."""
+    y: list[int] = []
+    i = 0 
+    while i < len(x):
+        if x[i] % 2 == 0: 
+            y.append(x[i])
+        i += 1
+    return y
+
+
+def sub(a_list: list[int], start: int, end: int) -> list[int]: 
+    """This function generates the values between the start index and end index of a list."""
+    y: list[int] = []
+    if len(a_list) == 0 or start >= len(a_list) or end <= 0: 
+        return y
+    end = end - 1
+    while start <= end:
+        if end >= len(a_list): 
+            end = len(a_list) - 1
+            y.append(a_list[start])
+        else: 
+            if start > 0: 
+                y.append(a_list[start])
+            else: 
+                start = 0
+                y.append(a_list[start])
+        start += 1
+    return y
+
+
+def concat(one: list[int], two: list[int]) -> list[int]:
+    """This list generates a new list that contains all of the elements of the first list followed by all of the elements of the second list."""
+    new_list: list[int] = []
+    i = 0
+    while i < len(one):
+        new_list.append(one[i])
+        i += 1
+    i = 0
+    while i < len(two):
+        new_list.append(two[i])
+        i += 1
+    return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,69 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730316359"
+
+
+def test_only_evens_empty() -> None:
+    """This tests if the function returns empty list when there are no even numbers."""
+    x: list[int] = [1, 3, 5, 7, 9]
+    assert only_evens(x) == []
+
+
+def test_only_evens_single_item() -> None:
+    """This tests if the function returns the only even number in the list."""
+    x: list[int] = [1, 3, 4, 5, 7]
+    assert only_evens(x) == [4]
+
+
+def test_only_evens_multiple_items() -> None:
+    """This tests if the function returns all even numbers in the list."""
+    x: list[int] = [2, 4, 6, 8, 9]
+    assert only_evens(x) == [2, 4, 6, 8]
+
+
+def test_sub_start_index_greater_than_or_equal_to_length_of_list() -> None:
+    """This tests if the function returns an empty list if the start index is greater than or equal to the length of the list."""
+    a_list: list[int] = [1, 2, 3, 4, 5]
+    start: int = 7
+    end: int = 9
+    assert sub(a_list, start, end) == []
+
+
+def test_sub_part_of_the_list() -> None:
+    """This tests if the function returns values in between the index values."""
+    a_list: list[int] = [1, 2, 3, 4, 5]
+    start: int = 1
+    end: int = 4
+    assert sub(a_list, start, end) == [2, 3, 4]
+
+
+def test_sub_whole_list() -> None:
+    """This tests if the function returns the whole list with the appropriate index values."""
+    a_list: list[int] = [1, 2, 3, 4, 5]
+    start: int = 0
+    end: int = 5
+    assert sub(a_list, start, end) == [1, 2, 3, 4, 5]
+
+
+def test_concat_two_empty_lists() -> None:
+    """This tests if the function returns an empty list when both lists are empty."""
+    one: list[int] = []
+    two: list[int] = []
+    assert concat(one, two) == []
+
+
+def test_concat_two_lists_of_same_length() -> None:
+    """This tests if the function generates both lists together when they are of equal length."""
+    one: list[int] = [1, 2, 3]
+    two: list[int] = [9, 8, 7]
+    assert concat(one, two) == [1, 2, 3, 9, 8, 7]
+
+
+def test_concat_two_lists_of_different_lengths() -> None:
+    """This tests if the function generates both lists together when they are of different lengths."""
+    one: list[int] = [1, 2, 3]
+    two: list[int] = [9]
+    assert concat(one, two) == [1, 2, 3, 9]
